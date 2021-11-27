04e2843d885b6f8b5c90c7ab903066649dad1962
Giovanni Cefalo <gcefalo@e4r7p1.42roma.it>
2021-11-27 19:18:58 +0100

    cpp07 done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <string>
 
@@ -5,20 +7,50 @@ template<typename T>
 class Array
 {
 public:
-    Array<T>(void) {};
-    Array<T>(unsigned int n) {arr = new [n] T}
-    Array<T>(Array<T> const &other)
+    Array(void) : len(0), arr(0) {};
+    Array(unsigned int n) : len(n) {arr = new T[n];}
+    Array(Array const &other)
     {
-        if (*this != other)
+        if (this != &other)
             *this = other;
     }
-    ~Array(void);
+    ~Array(void) {delete [] arr;}
 
     Array const &operator=(Array const &other)
     {
-
+        if (this != &other)
+        {
+            len = other.len;
+            if (arr)
+                delete [] arr;
+            arr = new T[len];
+            for (int i = 0; i < len; i++)
+            {
+                arr[i] = other.arr[i];
+            }
+        }
+        return (*this);
     }
 
+    T   &operator[](int i)
+    {
+        if (i < 0 || i >= size())
+            throw OutOfRangeException();
+        return (this->arr[i]);
+    }
+
+    int size(void) const {return (len);}
+
+    class OutOfRangeException : public std::exception
+    {
+        public:
+        const char *what() const throw()
+        {
+            return ("Cannot retrieve array element: Index is out of range");
+        }
+    };
+
 private:
-    T   *arr;
+    T               *arr;
+    unsigned int    len;
 };
